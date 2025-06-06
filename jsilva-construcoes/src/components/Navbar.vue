a42affc23ec81fdf6118d8f8464f6934fba457f4
KauanGab <auansozi@gmail.com>
2025-06-05 22:03:51 -0300

    Styles: removendo o css padrao

--- jsilva-construcoes/src/components/Navbar.vue
+++ jsilva-construcoes/src/components/Navbar.vue
@@ -1,14 +1,30 @@
-<!-- src/components/Navbar.vue -->
+
 <template>
-  <nav class="bg-blue-700 text-white p-4 flex gap-4">
-    <RouterLink to="/" class="hover:underline">Início</RouterLink>
-    <RouterLink to="/about" class="hover:underline">Sobre</RouterLink>
-    <RouterLink to="/services" class="hover:underline">Serviços</RouterLink>
-    <RouterLink to="/portfolio" class="hover:underline">Portfólio</RouterLink>
-    <RouterLink to="/contact" class="hover:underline">Contato</RouterLink>
+  <nav class="bg-[#1E3A8A] text-white p-4 flex justify-between items-center">
+
+    <div>
+      <RouterLink to="/">
+        <img src="../assets/jsilva_logo.png" alt="Jsilva Construções Logo" class="h-2 w-2">
+      </RouterLink>
+    </div>
+
+    <div class="flex gap-4">
+      <RouterLink to="/" class="hover:text-[#F97316] hover:underline px-3 py-2 rounded-md text-sm font-medium">Início</RouterLink>
+      <RouterLink to="/about" class="hover:text-[#F97316] hover:underline px-3 py-2 rounded-md text-sm font-medium">Sobre</RouterLink>
+      <RouterLink to="/services" class="hover:text-[#F97316] hover:underline px-3 py-2 rounded-md text-sm font-medium">Serviços</RouterLink>
+      <RouterLink to="/portfolio" class="hover:text-[#F97316] hover:underline px-3 py-2 rounded-md text-sm font-medium">Portfólio</RouterLink>
+      <RouterLink to="/contact" class="hover:text-[#F97316] hover:underline px-3 py-2 rounded-md text-sm font-medium">Contato</RouterLink>
+    </div>
   </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
+
+
 </script>
+
+<style scoped>
+
+</style>
+
