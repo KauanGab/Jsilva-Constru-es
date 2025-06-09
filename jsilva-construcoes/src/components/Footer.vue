bd737aef4120b266fc1577194efb4c333a601ea3
KauanGab <auansozi@gmail.com>
2025-06-08 23:59:03 -0300

    Styles: correção no boot do footer

--- jsilva-construcoes/src/components/Footer.vue
+++ jsilva-construcoes/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-blue-800 text-white text-center p-4">
+  <footer class="bg-blue-800 text-white text-left p-4 m-3">
     &copy; 2025 Jsilva Construções. Todos os direitos reservados para a empresa.
   </footer>
 </template>
