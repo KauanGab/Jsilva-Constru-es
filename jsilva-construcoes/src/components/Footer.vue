275a8fc67ab14a925022de24946be048eeed3188
KauanGab <auansozi@gmail.com>
2025-06-08 23:57:48 -0300

    Styles: correção no footer

--- jsilva-construcoes/src/components/Footer.vue
+++ jsilva-construcoes/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="bg-blue-800 text-white text-center p-4">
-    &copy; 2025 Jsilva Construções. Todos os direitos reservados.
+    &copy; 2025 Jsilva Construções. Todos os direitos reservados para a empresa.
   </footer>
 </template>
